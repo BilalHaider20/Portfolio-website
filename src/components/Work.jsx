15fc03c84492289f6be2dc2319ab089c8733b310
Bilal Haider <haiderbilal306@gmail.com>
2024-03-03 02:40:23 +0500

    added link to projets

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -17,7 +17,7 @@ function Work() {
                   <div className='opacity-0 group-hover:opacity-90 bg-[gray]/70 absolute inset-0 flex flex-col justify-center items-center'>
                       <span className='text-2xl font-bold text-white -tracking-wider'>Natour Project</span>
                       <div className='py-auto text-center'>
-                          <a href="#">
+                          <a href="https://bilalhaider20.github.io/Natour-Website/">
                               <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700
                               font-bold text-lg'>
                                   Live
